5bd79001099b1608104788132e34598e7ea66f38
near-you-luV <v.pelishenko1999@gmail.com>
2025-12-05 13:54:32 +0200

    changed video

--- frontend/src/pages/AboutPage.jsx
+++ frontend/src/pages/AboutPage.jsx
@@ -136,7 +136,7 @@ export default function AboutPage() {
             <iframe
               width="100%"
               height="400"
-              src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+              src="https://www.youtube.com/embed/jNQXAC9IVRw"
               title="YouTube video player"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
